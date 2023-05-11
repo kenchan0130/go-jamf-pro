f07bbd471cffba86f24ddaff5ffa68e7c6e5e299
kenchan0130 <tt.tanishi100@gmail.com>
2023-05-11 21:28:26 +0900

    remove unused methods

--- jamfproapi/client.go
+++ jamfproapi/client.go
@@ -5,8 +5,6 @@ package jamfproapi
 
 import (
 	"fmt"
-	"io"
-	"log"
 	"net/url"
 	"path"
 
@@ -64,17 +62,4 @@ func NewClient(serverURL string) (*Client, error) {
 	c.SSOFailover = (*SSOFailoverService)(&c.common)
 
 	return c, nil
-}
-
-// HandleCloseFunc can be used to close an io.ReadCloser with message
-func HandleCloseFunc(v io.ReadCloser, logger any) {
-	if err := v.Close(); err != nil {
-		l := logger.(*log.Logger)
-		if l != nil {
-			l.Printf("Error closing io: %v", err)
-			return
-		}
-
-		fmt.Printf("Error closing io: %v", err)
-	}
 }
